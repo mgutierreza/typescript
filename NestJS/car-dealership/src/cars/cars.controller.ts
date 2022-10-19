e69c5a188eba386742509d9e521da6e2d8b30efb
mgutierreza <mgutierrez6@gmail.com>
2022-10-19 13:16:33 -0500

    Generación de objetos controller y service
    Generación de objetos controller y service

--- NestJS/car-dealership/src/cars/cars.controller.ts
+++ NestJS/car-dealership/src/cars/cars.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-    private cars = ['Toyota','Kia','Nissan','Chevrolet','Hiunday'];
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id: string){
+        console.log({id})
+        return this.carsService.findOneById(+id);
     }
 }
---
+++ NestJS/car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+            {
+                id:1,
+                Marca:'Toyota',
+                Modelo:'Corolla'
+            },
+            {
+                id:2,
+                Marca:'Kia',
+                Modelo:'Model'
+            },
+            {
+                id:3,
+                Marca:'Nissan',
+                Modelo:'Sunny'
+            },
+            {
+                id:4,
+                Marca:'Honda',
+                Modelo:'Civic'
+            },
+            {
+                id:5,
+                Marca:'Hiunday',
+                Modelo:'Elantra'
+            },
+        ];
+
+    findAll(){
+        return this.cars;
+    }
+
+    findOneById(id: number){
+        const car = this.cars.find(car => car.id === id);
+        return car;
+    }
+}
