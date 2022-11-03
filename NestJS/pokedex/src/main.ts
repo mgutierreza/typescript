531f87377d4f552eab0639dc74eef6c82b4f8a63
mgutierreza <mgutierrez6@gmail.com>
2022-11-03 11:57:29 -0500

    Introducción a base de datos MongoDB
    Introducción a base de datos MongoDB

--- NestJS/pokedex/src/main.ts
+++ NestJS/pokedex/src/main.ts
@@ -1,9 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Validate } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v3')
+  
+  app.setGlobalPrefix('api/v3');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true, })
+    );
+
   await app.listen(3000);
 }
 bootstrap();
--- NestJS/pokedex/src/pokemon/entities/pokemon.entity.ts
+++ NestJS/pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,13 @@
-export class Pokemon {}
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document} from 'mongoose';
+@Schema()
+export class Pokemon extends Document {
+    
+    @Prop({unique:true,index:true,})
+    nombre:string;
+    
+    @Prop({unique:true,index:true,})
+    numero:number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
